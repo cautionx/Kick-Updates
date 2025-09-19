b4a17dcc58c1ed655368f70a89a13ba0ef873089
caution <jhannan644@gmail.com>
2025-09-19 03:41:07 +0100

    Update Ready.js

--- Events/Ready.js
+++ Events/Ready.js
@@ -25,7 +25,7 @@ module.exports = {
     try {
       console.log("🔄 Refreshing application commands...");
       await rest.put(
-        Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+        Routes.applicationCommands(process.env.CLIENT_ID),
         { body: commands }
       );
       console.log("✅ Slash commands registered.");
